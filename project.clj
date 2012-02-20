273e3afac69947438cabdddcd2e85cee7b464e80
Roman Gonzalez <romanandreg@gmail.com>
2012-02-19 17:06:00 -0800

    First release of the http-river library

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.van-clj/http-river "0.0.1-SNAPSHOT"
+(defproject org.van-clj/http-river "0.0.1"
   :description "Stream HTTP response bodies with river."
   :author "Roman Gonzalez"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.van-clj/river "0.1.0-SNAPSHOT"]
+                 [org.van-clj/river "0.1.0"]
                  [clj-http "0.3.2"]])
